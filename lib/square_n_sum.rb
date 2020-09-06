d61a4c1a4cf7d424217d7f7c87db6ddbdfcbb0e0
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-09-06 21:26:36 +0100

    Refactor 2.0.

--- lib/square_n_sum.rb
+++ lib/square_n_sum.rb
@@ -1,4 +1,3 @@
 def square_sum(numbers)
-  return 0 if numbers.nil? || numbers == []
-  numbers.inject(0) { |sum,num| sum + (num**2) }
+  numbers.nil? || numbers == [] ? 0 : numbers.inject(0) { |sum,num| sum + (num**2) }
 end
